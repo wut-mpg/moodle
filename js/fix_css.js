7fd43a72ae8ea244c6f45a3bc9553ab3e14f40d9
Maciej Stefańczyk <maciek.slon@gmail.com>
2020-10-09 17:54:34 +0200

    Update fix_css.js

--- js/fix_css.js
+++ js/fix_css.js
@@ -1,5 +1,16 @@
-function fix_links(parent_name = "book_conent") {
+function fix_links(parent_name = "book_content") {
 	var content = document.getElementsByClassName(parent_name);
+	var links = content[0].getElementsByTagName('a');
+
+	for (var i = 0; i < links.length; i++) {
+	  if (links[i].title == "TeX") continue;
+	  links[i].classList.add("text-info");
+	  console.log(links[i]);
+	}
+}
+
+function fix_links_id(parent_name = "book_content") {
+	var content = document.getElementById(parent_name);
 	var links = content[0].getElementsByTagName('a');
 
 	for (var i = 0; i < links.length; i++) {
